8300979a6d6c9ba87c25adc956a92dcc409633ac
Sam <57677512+threesam@users.noreply.github.com>
2023-06-25 13:24:56 -0400

    adds tags

--- apps/sanity/sanity.config.ts
+++ apps/sanity/sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {tags} from 'tags'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: 'qcht0vh1',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), tags(), visionTool()],
 
   schema: {
     types: schemaTypes,
